ca31884f61190e28a9ad8e2f265862b8574a4791
althaf-m <17941817+althaf-m@users.noreply.github.com>
2022-12-13 01:08:39 +0530

    Traits

---
+++ 03-traits/trait.php
@@ -0,0 +1,21 @@
+<?php
+
+trait Log
+{
+    protected function log($message)
+    {
+        echo "$message\n";
+    }
+}
+
+class Table
+{
+    use Log;
+
+    public function save()
+    {
+        $this->log('save start');
+    }
+}
+
+(new Table())->save();
